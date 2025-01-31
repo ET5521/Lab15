e901aabfca4489d47606516f489f62ee01a93e47
Thanaphat <earth12345523@gmail.com>
2025-01-31 15:27:03 +0700

    push1

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -2,12 +2,20 @@
 #include <algorithm>
 using namespace std;
 
-int main(){
+int main()
+{
 	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
+
 	cout << "Before: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	sort(&data[0],&data[10]);
-	cout << "\nAfter: ";
+	cout << endl;
+
+	sort (data, data + 5);
+	sort (data + 5, data + 10);
+
+	cout << "After: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
+	cout << endl;
+
 	return 0;
 }
